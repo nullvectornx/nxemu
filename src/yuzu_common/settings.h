7440e89ef05ae2e06659fe42bfd4bd67e2ceeca2
N3xoX1 <n3xox1@nxemu.com>
2026-06-17 19:45:38 +0930

    yuzu_common: remove enable_telemetry

--- src/yuzu_common/settings.h
+++ src/yuzu_common/settings.h
@@ -158,7 +158,6 @@ struct Values {
                                            Category::Network};
 
     // WebService
-    Setting<bool> enable_telemetry{linkage, true, "enable_telemetry", Category::WebService};
     Setting<std::string> web_api_url{linkage, "https://api.yuzu-emu.org", "web_api_url",
                                      Category::WebService};
     Setting<std::string> yuzu_username{linkage, std::string(), "yuzu_username",
